e215fd2a560f0a2c4a57b6dd7acdcba2f6baaf47
Kritagya <krishmalhotra9899@gmail.com>
2026-04-20 23:16:36 +0530

    Created store urls and linked to project

--- voltix_backend/urls.py
+++ voltix_backend/urls.py
@@ -1,9 +1,10 @@
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include # Make sure 'include' is here
 
 urlpatterns = [
-    # ... your existing paths ...
-] 
+    path('admin/', admin.site.urls),
+    path('', include('store.urls')), # This tells Django to look in your store app for the home page
+]
 
 # Add this at the end to allow image viewing during development
 if settings.DEBUG:
